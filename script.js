88236415853628d298dbc3748960bcbd09a4b95a
Manzar A. <93775160+Manzar78@users.noreply.github.com>
2024-09-26 13:52:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleForms() {
+    const signInForm = document.getElementById("signInForm");
+    const signUpForm = document.getElementById("signUpForm");
+    
+    signInForm.classList.toggle("hidden");
+    signUpForm.classList.toggle("hidden");
+}
